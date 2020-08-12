2d0caa01872bda8cf166e8d0145e1a49f06d3308
zacwoll <zacwoll@gmail.com>
2020-08-12 00:49:58 +0000

    added a try-except KeyError to remove the _sa_instance_state key if exists

--- models/base_model.py
+++ models/base_model.py
@@ -54,6 +54,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if dictionary['_sa_instance_state']:
+        try:
             del dictionary['_sa_instance_state']
+        except KeyError:
+            pass
         return dictionary
