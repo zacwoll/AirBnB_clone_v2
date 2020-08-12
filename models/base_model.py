0d33b662d8876e6fc3b83d42f5676e3f93b2ea73
zacwoll <zacwoll@gmail.com>
2020-08-12 20:53:43 +0000

    removed _sa_instance_state from the object on stringification

--- models/base_model.py
+++ models/base_model.py
@@ -38,8 +38,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                     self.to_dict())
 
     def new(self, obj):
         """Add object to current database session"""
