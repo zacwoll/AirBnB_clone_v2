56d42e7b676091c34d1e0d8bc401251f8f963593
zacwoll <zacwoll@gmail.com>
2020-08-18 23:50:28 +0000

    no idea why it can't commit to two or three forward slashes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-grep -q 'location /hbnb_static' /etc/nginx/sites-available/default || sed -i '/^server {/a \\\tlocation /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+grep -q 'location /hbnb_static' /etc/nginx/sites-available/default || sed -i '/^server {/a \\tlocation /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 service nginx restart
