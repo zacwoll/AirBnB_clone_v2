9a280b501ef7a975157330e1f36672dfd928984f
zacwoll <zacwoll@gmail.com>
2020-08-18 22:27:28 +0000

    added a slash to make the link work properly

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ apt-get -y update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 grep -q 'location /hbnb_static' /etc/nginx/sites-available/default || sed -i '/server_name _;/a \\tlocation /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 service nginx restart
