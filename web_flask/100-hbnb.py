57f1e0270b14fc4d293fa64300a0284178bf5d99
zacwoll <zacwoll@gmail.com>
2020-08-31 20:37:13 +0000

    last part of project, added a lot of nonsense .css, the given files didn't work so I took some from the old project

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 app = Flask(__name__)
 
 
@@ -17,7 +18,8 @@ def get_states(id='0'):
     """ Display HTML page with list of states """
     return render_template('10-hbnb_filters.html',
                            states=storage.all(State).values(),
-                           amenities=storage.all(Amenity).values(), id=id)
+                           amenities=storage.all(Amenity).values(),
+                           places=storage.all(Place).values(), id=id)
 
 
 if __name__ == '__main__':
